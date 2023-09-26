e99e14efcbf7d2d416982b7fe171cec0531cdc59
Sergey <semakin.s.v@gmail.com>
2023-09-26 14:53:22 +0300

    added tasks 1 and 2

---
+++ src/task1/Task1.java
@@ -0,0 +1,45 @@
+package task1;
+
+class Answer {
+    public static void arrayOutOfBoundsException() {
+        int[] arr = {1,2,3,4};
+        System.out.println(arr[4]);
+        // Напишите свое решение ниже
+
+    }
+
+    public static void divisionByZero() {
+        int a = 1 / 0;
+        // Напишите свое решение ниже
+
+    }
+
+    public static void numberFormatException() {
+        Integer.parseInt("Hello");
+        // Напишите свое решение ниже
+
+    }
+}
+
+public class Task1 {
+    public static void main(String[] args) {
+        Answer ans = new Answer();
+        try {
+            ans.arrayOutOfBoundsException();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Выход за пределы массива");
+        }
+
+        try {
+            ans.divisionByZero();
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на ноль");
+        }
+
+        try {
+            ans.numberFormatException();
+        } catch (NumberFormatException e) {
+            System.out.println("Ошибка преобразования строки в число");
+        }
+    }
+}
